b2718de77105637391bfbba04f5036793285ffad
Shang Zhendan <Z.Shang@rbht.nhs.uk>
2022-04-17 21:24:48 +0100

    Change accuracy_vs_precision to basic_concepts

--- _build/jupyter_execute/temporary.py
+++ _build/jupyter_execute/temporary.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[8]:
 
 
-import numpy as np 
-import matplotlib.pyplot as plt
-import math
 
-x = np.linspace(0, 10, 1000)
-y = np.floor(x)
-
-plt.style.use('dark_background')
-plt.plot(x, y)
-plt.show()
 
